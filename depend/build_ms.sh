9c9c20a6be181e28147580796121977f92307cf5
xiaoming <2014500726@smail.xtu.edu.cn>
2023-08-02 16:22:10 +0800

    ci:fix windows build
    Signed-off-by: xiaoming <2014500726@smail.xtu.edu.cn>

--- depend/build_ms.sh
+++ depend/build_ms.sh
@@ -5,7 +5,7 @@ SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 tar -xzvf zlib-1.2.11.tar.gz
 cd $SHELL_FOLDER/zlib-1.2.11
 make -f win32/Makefile.gcc install \
- INCLUDE_PATH=/d/qfsviewer/include \
- LIBRARY_PATH=/d/qfsviewer/lib \
- BINARY_PATH=/d/qfsviewer/bin 
+ INCLUDE_PATH=/d/zlib/include \
+ LIBRARY_PATH=/d/zlib/lib \
+ BINARY_PATH=/d/zlib/bin 
 rm -rf $SHELL_FOLDER/zlib-1.2.11
